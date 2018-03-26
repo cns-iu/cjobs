2578d7ec2038ab095553098fb8c7786ded5b43b9
obscrivn <obscrivn@indiana.edu>
2018-03-26 11:55:26 -0400

    remove uzzi from workflow

--- scripts/extract-month-year-job-skills.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE skills_jobs_ds_de_count_month AS (SELECT count(DISTINCT maindes.bgtjobid) as jobs,
-count(DISTINCT skillsdes.skill) as skills, extract(month from maindes.jobdate) as mon,
- extract(year from maindes.jobdate) as yyyy, bgtoccname  
-FROM maindes, skillsdes WHERE maindes.bgtjobid = skillsdes.bgtjobid 
-//AND skillsdes.skillcluster NOT LIKE 'na'
-GROUP by 5,4,3 order by 4,3,5 limit 4; );
